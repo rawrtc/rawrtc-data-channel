131c45ed4f7db05c9c47a303b64a65550aca04d1
Lennart Grahl <lennart.grahl@gmail.com>
2018-03-09 20:56:39 +0100

    Update SCTP redirect transport to work with the current codebase and make it non-blocking
    Add `rawrtc_sctp_redirect_transport_state_change_handler`
    Add `rawrtc_sctp_redirect_transport_feed_inbound` function
    Add `rawrtc_sctp_redirect_transport_get_state` function
    Add `rawrtc_sctp_redirect_transport_state_to_name` function
    Fix also handle EWOULDBLOCK if it exists

--- src/librawrtcdc/sctp_redirect_transport.c
+++ src/librawrtcdc/sctp_redirect_transport.c
@@ -5,7 +5,7 @@
 #include <unistd.h> // close
 #include <errno.h>
 #include <rawrtcdc.h>
-#include "dtls_transport.h"
+#include "crc32c.h"
 #include "sctp_redirect_transport.h"
 
 #define DEBUG_MODULE "redirect-transport"
@@ -45,7 +45,7 @@ static void patch_sctp_header(
     memset(mbuf_buf(buffer), 0, 4);
     mbuf_set_pos(buffer, start);
     // Recalculate checksum
-    checksum = calculate_crc32c(0, mbuf_buf(buffer), mbuf_get_left(buffer));
+    checksum = rawrtc_crc32c(0x00000000, mbuf_buf(buffer), mbuf_get_left(buffer));
     // Advance to checksum field, set it and rewind back
     mbuf_advance(buffer, 8);
     err = mbuf_write_u32(buffer, checksum);
@@ -61,10 +61,10 @@ static void patch_sctp_header(
  */
 static void redirect_from_raw(
         int flags,
-        void* arg
+        void* arg // not checked
 ) {
     struct rawrtc_sctp_redirect_transport* const transport = arg;
-    struct mbuf* buffer;
+    struct mbuf* const buffer = transport->buffer;
     enum rawrtc_code error;
     struct sockaddr_in from_address;
     socklen_t address_length;
@@ -74,40 +74,48 @@ static void redirect_from_raw(
     uint16_t source;
     uint16_t destination;
 
+    // Detached?
+    if (transport->flags & RAWRTC_SCTP_REDIRECT_TRANSPORT_FLAGS_DETACHED) {
+        DEBUG_PRINTF("Ignoring SCTP packet ready event, transport is detached\n");
+        return;
+    }
+
     if ((flags & FD_READ) == FD_READ) {
-        buffer = transport->buffer;
-
-        // Rewind buffer
-        mbuf_rewind(buffer);
-
         // Receive
         address_length = sizeof(from_address);
         length = recvfrom(transport->socket, mbuf_buf(buffer), mbuf_get_space(buffer),
                 0, (struct sockaddr*) &from_address, &address_length);
         if (length == -1) {
-            DEBUG_WARNING("Unable to receive raw message: %m\n", errno);
-            return;
+            switch (errno) {
+                case EAGAIN:
+#if (defined(EWOULDBLOCK) && EAGAIN != EWOULDBLOCK)
+                case EWOULDBLOCK:
+#endif
+                    break;
+                default:
+                    DEBUG_WARNING("Unable to receive raw message: %m\n", errno);
+                    break;
+            }
+            goto out;
         }
         mbuf_set_end(buffer, (size_t) length);
-
-        // TODO: Receive remaining bytes (if any)
 
         // Check address
         error = rawrtc_error_to_code(sa_set_sa(&from, (struct sockaddr*) &from_address));
         if (error) {
             DEBUG_WARNING("Invalid sender address: %m\n", error);
-            return;
+            goto out;
         }
         DEBUG_PRINTF("Received %zu bytes via RAW from %j\n", mbuf_get_left(buffer), &from);
         if (!sa_isset(&from, SA_ADDR) && !sa_cmp(&transport->redirect_address, &from, SA_ADDR)) {
             DEBUG_WARNING("Ignoring data from unknown address");
-            return;
+            goto out;
         }
 
         // Skip IPv4 header
         header_length = (size_t) (mbuf_read_u8(buffer) & 0xf);
         mbuf_advance(buffer, -1);
-        DEBUG_PRINTF("RAW IPv4 header length: %zu\n", header_length);
+        DEBUG_PRINTF("IPv4 header length: %zu\n", header_length);
         mbuf_advance(buffer, header_length * 4);
 
         // Read source and destination port
@@ -115,57 +123,29 @@ static void redirect_from_raw(
         destination = ntohs(mbuf_read_u16(buffer));
         sa_set_port(&from, source);
         (void) destination;
-        DEBUG_PRINTF("RAW from %J to %"PRIu16"\n", &from, destination);
+        DEBUG_PRINTF("Raw from %J to %"PRIu16"\n", &from, destination);
         mbuf_advance(buffer, -4);
 
         // Is this from the correct source?
         if (source != sa_port(&transport->redirect_address)) {
-            DEBUG_WARNING("Ignored data from different source\n");
-            return;
+            DEBUG_PRINTF("Ignored data from different source port %"PRIu16"\n", source);
+            goto out;
         }
 
         // Update SCTP header with changed ports
         patch_sctp_header(buffer, transport->local_port, transport->remote_port);
 
-        // Send data
-        error = rawrtc_dtls_transport_send(transport->dtls_transport, buffer);
+        // Pass to outbound handler
+        error = transport->context.outbound_handler(buffer, 0x00, 0x00, transport->context.arg);
         if (error) {
-            DEBUG_WARNING("Could not send, error: %m\n", error);
-            return;
+            DEBUG_WARNING("Could not send packet, reason: %s\n", rawrtc_code_to_str(error));
+            goto out;
         }
     }
-}
 
-/*
- * Handle incoming messages (that are sent out via the raw socket).
- */
-static void redirect_to_raw(
-        struct mbuf* const buffer,
-        void* const arg
-) {
-    struct rawrtc_sctp_redirect_transport* const transport = arg;
-    ssize_t length;
-
-    // Check state
-    if (transport->state != RAWRTC_SCTP_REDIRECT_TRANSPORT_STATE_OPEN) {
-        DEBUG_NOTICE("Ignored packet of %zu bytes as transport is not open\n",
-                     mbuf_get_left(buffer));
-        return;
-    }
-
-    // Update SCTP header with changed ports
-    patch_sctp_header(buffer, transport->local_port, sa_port(&transport->redirect_address));
-
-    // Send over raw socket
-    DEBUG_PRINTF("Redirecting message (%zu bytes) to %J\n",
-            mbuf_get_left(buffer), &transport->redirect_address);
-    length = sendto(transport->socket, mbuf_buf(buffer), mbuf_get_left(buffer), 0,
-            &transport->redirect_address.u.sa, transport->redirect_address.len);
-    if (length == -1) {
-        DEBUG_WARNING("Unable to redirect message: %m\n", errno);
-        return;
-    }
-    mbuf_advance(buffer, length);
+out:
+    // Rewind buffer
+    mbuf_rewind(buffer);
 }
 
 /*
@@ -186,15 +166,31 @@ static void set_state(
             }
         }
 
-        // Remove from DTLS transport
-        // Note: No NULL checking needed as the function will do that for us
-        rawrtc_dtls_transport_clear_data_transport(transport->dtls_transport);
+        // Mark as detached & detach from DTLS transport
+        transport->flags |= RAWRTC_SCTP_REDIRECT_TRANSPORT_FLAGS_DETACHED;
+        if (transport->context.detach_handler) {
+            transport->context.detach_handler(transport->context.arg);
+        }
     }
 
     // Set state
     transport->state = state;
 
-    // TODO: Raise event
+    // Call handler (if any)
+    if (transport->state_change_handler) {
+        transport->state_change_handler(state, transport->arg);
+    }
+}
+
+static enum rawrtc_code validate_context(
+        struct rawrtc_sctp_transport_context* const context // not checked
+) {
+    // Ensure the context contains all necessary callbacks
+    if (!context->state_getter || !context->outbound_handler || !context->detach_handler) {
+        return RAWRTC_CODE_INVALID_ARGUMENT;
+    } else {
+        return RAWRTC_CODE_SUCCESS;
+    }
 }
 
 /*
@@ -208,44 +204,69 @@ static void rawrtc_sctp_redirect_transport_destroy(
     // Stop transport
     rawrtc_sctp_redirect_transport_stop(transport);
 
+    // Call 'destroyed' handler (if fully initialised)
+    if (transport->flags & RAWRTC_SCTP_REDIRECT_TRANSPORT_FLAGS_INITIALIZED
+        && transport->context.destroyed_handler) {
+        transport->context.destroyed_handler(transport->context.arg);
+    }
+
     // Un-reference
-    mem_deref(transport->dtls_transport);
     mem_deref(transport->buffer);
 }
 
 /*
- * Create an SCTP redirect transport.
+ * Create an SCTP redirect transport from an external DTLS transport.
  * `*transportp` must be unreferenced.
+ *
+ * `redirect_ip` must be a IPv4 address.
+ *
+ * Important: The redirect transport requires to be run inside re's
+ *            event loop (`re_main`).
  */
-enum rawrtc_code rawrtc_sctp_redirect_transport_create(
+enum rawrtc_code rawrtc_sctp_redirect_transport_create_from_external(
         struct rawrtc_sctp_redirect_transport** const transportp, // de-referenced
-        struct rawrtc_dtls_transport* const dtls_transport, // referenced
+        struct rawrtc_sctp_transport_context* const context, // copied
         uint16_t const port, // zeroable
         char* const redirect_ip, // copied
-        uint16_t const redirect_port
+        uint16_t const redirect_port,
+        rawrtc_sctp_redirect_transport_state_change_handler* const state_change_handler, // nullable
+        void* const arg // nullable
 ) {
-    bool have_data_transport;
-    struct rawrtc_sctp_redirect_transport* transport;
     enum rawrtc_code error;
+    struct sa redirect_address;
+    enum rawrtc_external_dtls_transport_state dtls_transport_state;
+    struct rawrtc_sctp_redirect_transport* transport;
 
     // Check arguments
-    if (!transportp || !dtls_transport || !redirect_ip || redirect_port == 0) {
+    if (!transportp || !context || !redirect_ip || redirect_port == 0) {
         return RAWRTC_CODE_INVALID_ARGUMENT;
     }
 
-    // Check DTLS transport state
-    if (dtls_transport->state == RAWRTC_DTLS_TRANSPORT_STATE_CLOSED
-        || dtls_transport->state == RAWRTC_DTLS_TRANSPORT_STATE_FAILED) {
-        return RAWRTC_CODE_INVALID_STATE;
-    }
-
-    // Check if a data transport is already registered
-    error = rawrtc_dtls_transport_have_data_transport(&have_data_transport, dtls_transport);
+    // Ensure it's an IPv4 address
+    error = rawrtc_error_to_code(sa_set_str(
+            &redirect_address, redirect_ip, redirect_port));
     if (error) {
         return error;
     }
-    if (have_data_transport) {
+    if (sa_af(&redirect_address) != AF_INET) {
         return RAWRTC_CODE_INVALID_ARGUMENT;
+    }
+
+    // Ensure the context contains all necessary callbacks
+    error = validate_context(context);
+    if (error) {
+        return error;
+    }
+
+    // Check DTLS transport state
+    error = context->state_getter(&dtls_transport_state, context->arg);
+    if (error) {
+        DEBUG_WARNING("Getting external DTLS transport state failed: %s\n",
+                      rawrtc_code_to_str(error));
+        return RAWRTC_CODE_EXTERNAL_ERROR;
+    }
+    if (dtls_transport_state == RAWRTC_EXTERNAL_DTLS_TRANSPORT_STATE_CLOSED_OR_FAILED) {
+        return RAWRTC_CODE_INVALID_STATE;
     }
 
     // Allocate
@@ -255,17 +276,16 @@ enum rawrtc_code rawrtc_sctp_redirect_transport_create(
     }
 
     // Set fields
+    transport->context = *context;
+    transport->flags = 0;
     transport->state = RAWRTC_SCTP_REDIRECT_TRANSPORT_STATE_NEW;
-    transport->dtls_transport = mem_ref(dtls_transport);
-    transport->local_port = port ? port : RAWRTC_SCTP_REDIRECT_TRANSPORT_DEFAULT_PORT;
-    error = rawrtc_error_to_code(sa_set_str(
-            &transport->redirect_address, redirect_ip, redirect_port));
-    if (error) {
-        goto out;
-    }
+    transport->local_port = port > 0 ? port : RAWRTC_SCTP_REDIRECT_TRANSPORT_DEFAULT_PORT;
+    transport->redirect_address = redirect_address;
+    transport->state_change_handler = state_change_handler;
+    transport->arg = arg;
 
     // Create buffer
-    transport->buffer = mbuf_alloc(2048);
+    transport->buffer = mbuf_alloc(RAWRTC_SCTP_REDIRECT_TRANSPORT_RAW_SOCKET_RECEIVE_SIZE);
     if (!transport->buffer) {
         error = RAWRTC_CODE_NO_MEMORY;
         goto out;
@@ -278,14 +298,19 @@ enum rawrtc_code rawrtc_sctp_redirect_transport_create(
         goto out;
     }
 
-    // TODO: Set non-blocking
+    // Set non-blocking
+    error = rawrtc_error_to_code(net_sockopt_blocking_set(transport->socket, false));
+    if (error) {
+        goto out;
+    }
 
 out:
     if (error) {
         mem_deref(transport);
     } else {
-        // Set pointer
+        // Set pointer & mark as initialised
         *transportp = transport;
+        transport->flags |= RAWRTC_SCTP_REDIRECT_TRANSPORT_FLAGS_INITIALIZED;
     }
     return error;
 }
@@ -325,13 +350,6 @@ enum rawrtc_code rawrtc_sctp_redirect_transport_start(
         goto out;
     }
 
-    // Attach to ICE transport
-    error = rawrtc_dtls_transport_set_data_transport(
-            transport->dtls_transport, redirect_to_raw, transport);
-    if (error) {
-        goto out;
-    }
-
     // Update state & done
     set_state(transport, RAWRTC_SCTP_REDIRECT_TRANSPORT_STATE_OPEN);
     error = RAWRTC_CODE_SUCCESS;
@@ -356,12 +374,95 @@ enum rawrtc_code rawrtc_sctp_redirect_transport_stop(
     }
 
     // Check state
-    if (transport->state == RAWRTC_SCTP_REDIRECT_TRANSPORT_STATE_CLOSED) {
+    if (transport->flags & RAWRTC_SCTP_REDIRECT_TRANSPORT_FLAGS_DETACHED
+        || transport->state == RAWRTC_SCTP_REDIRECT_TRANSPORT_STATE_CLOSED) {
         return RAWRTC_CODE_SUCCESS;
     }
 
     // Update state
     set_state(transport, RAWRTC_SCTP_REDIRECT_TRANSPORT_STATE_CLOSED);
+    return RAWRTC_CODE_SUCCESS;
+}
+
+/*
+ * Feed inbound data to the SCTP redirect transport (that will be sent
+ * out via the raw socket).
+ *
+ * `buffer` contains the data to be fed to the raw transport. Since
+ * the data is not going to be referenced, you can pass a *fake* `mbuf`
+ * structure that hasn't been allocated with `mbuf_alloc` to avoid
+ * copying.
+ *
+ * Return `RAWRTC_CODE_INVALID_STATE` in case the transport is closed.
+ * In case the buffer could not be sent due to the raw socket's buffer
+ * being too full, `RAWRTC_CODE_TRY_AGAIN_LATER` will be returned. You
+ * can safely ignore this code since SCTP will retransmit data on a
+ * reliable stream.
+ * Otherwise, `RAWRTC_CODE_SUCCESS` is being returned.
+ */
+enum rawrtc_code rawrtc_sctp_redirect_transport_feed_inbound(
+        struct rawrtc_sctp_redirect_transport* const transport,
+        struct mbuf* const buffer
+) {
+    ssize_t length;
+
+    // Check arguments
+    if (!transport || !buffer) {
+        return RAWRTC_CODE_INVALID_ARGUMENT;
+    }
+
+    // Detached?
+    if (transport->flags & RAWRTC_SCTP_REDIRECT_TRANSPORT_FLAGS_DETACHED) {
+        return RAWRTC_CODE_INVALID_STATE;
+    }
+
+    // Check state
+    if (transport->state != RAWRTC_SCTP_REDIRECT_TRANSPORT_STATE_OPEN) {
+        // Note: Silently ignore inbound packets received as long as the transport is not open.
+        return RAWRTC_CODE_SUCCESS;
+    }
+
+    // Update SCTP header with changed ports
+    patch_sctp_header(buffer, transport->local_port, sa_port(&transport->redirect_address));
+
+    // Send over raw socket
+    DEBUG_PRINTF("Redirecting message (%zu bytes) to %J\n",
+                 mbuf_get_left(buffer), &transport->redirect_address);
+    length = sendto(transport->socket, mbuf_buf(buffer), mbuf_get_left(buffer), 0,
+                    &transport->redirect_address.u.sa, transport->redirect_address.len);
+    if (length == -1) {
+        switch (errno) {
+            case EAGAIN:
+#if (defined(EWOULDBLOCK) && EAGAIN != EWOULDBLOCK)
+                case EWOULDBLOCK:
+#endif
+                // We're just dropping the packets in this case. SCTP will retransmit eventually.
+                return RAWRTC_CODE_TRY_AGAIN_LATER;
+            default:
+                DEBUG_WARNING("Unable to redirect message: %m\n", errno);
+                return rawrtc_error_to_code(errno);
+        }
+    }
+
+    // Advance buffer & done
+    mbuf_advance(buffer, length);
+    return RAWRTC_CODE_SUCCESS;
+}
+
+/*
+ * Get the state of the SCTP redirect transport.
+ */
+enum rawrtc_code rawrtc_sctp_redirect_transport_get_state(
+        enum rawrtc_sctp_redirect_transport_state* const statep, // de-referenced
+        struct rawrtc_sctp_redirect_transport* const transport
+) {
+    // Check arguments
+    if (!statep || !transport) {
+        return RAWRTC_CODE_INVALID_ARGUMENT;
+    }
+
+    // Set state & done
+    *statep = transport->state;
     return RAWRTC_CODE_SUCCESS;
 }
 
@@ -377,9 +478,25 @@ enum rawrtc_code rawrtc_sctp_redirect_transport_get_port(
         return RAWRTC_CODE_INVALID_ARGUMENT;
     }
 
-    // Set port
+    // Set port & done
     *portp = transport->local_port;
-
-    // Done
     return RAWRTC_CODE_SUCCESS;
+}
+
+/*
+ * Get the corresponding name for an SCTP redirect transport state.
+ */
+char const * const rawrtc_sctp_redirect_transport_state_to_name(
+        enum rawrtc_sctp_redirect_transport_state const state
+) {
+    switch (state) {
+        case RAWRTC_SCTP_REDIRECT_TRANSPORT_STATE_NEW:
+            return "new";
+        case RAWRTC_SCTP_REDIRECT_TRANSPORT_STATE_OPEN:
+            return "open";
+        case RAWRTC_SCTP_REDIRECT_TRANSPORT_STATE_CLOSED:
+            return "closed";
+        default:
+            return "???";
+    }
 }
