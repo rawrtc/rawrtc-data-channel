9cef736d7005276463bf98733ac3657b14574b68
Lennart Grahl <lennart.grahl@gmail.com>
2019-02-25 22:32:54 +0100

    Refactor SSE4.2 detection for hardware CRC32-C and software fallback

--- src/crc32c/crc32c.h
+++ src/crc32c/crc32c.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <re.h>
 
-uint32_t rawrtc_crc32c(uint32_t crc, void const* buffer, size_t length);
-
 void rawrtc_crc32c_init(void);
+
+uint32_t rawrtc_crc32c(void const* buffer, size_t length);
