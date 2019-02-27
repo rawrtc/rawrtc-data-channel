ae7920a0b101263e63148f4102b4f5e1d35ba2f0
Lennart Grahl <lennart.grahl@gmail.com>
2019-02-27 23:40:05 +0100

    Re-introduce rawrtc_data_transport_get_transport

--- include/rawrtcdc/data_transport.h
+++ include/rawrtcdc/data_transport.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <rawrtcc/code.h>
+#include <re.h>
 
 /**
  * Data transport type.
@@ -11,6 +13,15 @@ enum rawrtc_data_transport_type {
  * Generic data transport.
  */
 struct rawrtc_data_transport;
+
+/**
+ * Get the data transport's type and underlying transport reference.
+ * `*internal_transportp` must be unreferenced.
+ */
+enum rawrtc_code rawrtc_data_transport_get_transport(
+    enum rawrtc_data_transport_type* const typep,  // de-referenced
+    void** const internal_transportp,  // de-referenced
+    struct rawrtc_data_transport* const transport);
 
 /**
  * Translate a data transport type to str.
