3e50631a983c98a6fbb80a9e2e34e814c43a7a5d
Lennart Grahl <lennart.grahl@gmail.com>
2019-02-27 23:25:16 +0100

    Expose rawrtc_sctp_capabilities_debug

--- src/sctp_capabilities/capabilities.h
+++ src/sctp_capabilities/capabilities.h
@@ -4,6 +4,3 @@
 struct rawrtc_sctp_capabilities {
     uint64_t max_message_size;
 };
-
-int rawrtc_sctp_capabilities_debug(
-    struct re_printf* const pf, struct rawrtc_sctp_capabilities const* const capabilities);
